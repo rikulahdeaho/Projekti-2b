1d341ed60b58c48e0b54d95fbacc718187440d19
rikulahdeaho <38558644+rikulahdeaho@users.noreply.github.com>
2023-11-28 20:00:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,39 @@
-const getJokeBtn = document.getElementById('getJokeBtn');
-const jokeSetup = document.getElementById('jokeSetup');
-const jokeDelivery = document.getElementById('jokeDelivery');
+$(document).ready(function () {
+  $('#getJokeBtn').click(function () {
+    const programmingCheckbox = $('#programmingCheckbox');
+    const christmasCheckbox = $('#christmasCheckbox');
 
-getJokeBtn.addEventListener('click', () => {
-  const programmingCheckbox = document.getElementById('programmingCheckbox');
-  const christmasCheckbox = document.getElementById('christmasCheckbox');
+    // Build the categories based on selected checkboxes
+    const selectedCategories = [];
+    if (programmingCheckbox.prop('checked')) {
+      selectedCategories.push('Programming');
+    }
+    if (christmasCheckbox.prop('checked')) {
+      selectedCategories.push('Christmas');
+    }
 
-  // Katsotaan valitut kategoriat
-  const selectedCategories = [];
-  if (programmingCheckbox.checked) {
-    selectedCategories.push('Programming');
-  }
-  if (christmasCheckbox.checked) {
-    selectedCategories.push('Christmas');
-  }
+    // Check if at least one category is selected
+    if (selectedCategories.length > 0) {
+      const apiUrl = https://v2.jokeapi.dev/joke/${selectedCategories.join()}?blacklistFlags=nsfw,racist,sexist&type=twopart;
 
-  // kategorian checkaus
-  if (selectedCategories.length > 0) {
-    const apiUrl = `https://v2.jokeapi.dev/joke/${selectedCategories.join()}?blacklistFlags=nsfw,racist,sexist&type=twopart`;
-
-    //Api kutsu
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        if (data.error) {
-          jokeSetup.textContent = `Failed to fetch joke. Please try again.`;
-          jokeDelivery.textContent = '';
-        } else {
-          jokeSetup.textContent = data.setup;
-          jokeDelivery.textContent = data.delivery;
-        }
-      })
-      .catch(error => {
-        jokeSetup.textContent = 'An error occurred while fetching the joke.';
-        jokeDelivery.textContent = '';
-        console.error('Error fetching joke:', error);
-      });
-  } else {
-    jokeSetup.textContent = 'Please select at least one category.';
-    jokeDelivery.textContent = '';
-  }
+      $.getJSON(apiUrl)
+        .done(function (data) {
+          if (data.error) {
+            $('#jokeSetup').text('Failed to fetch joke. Please try again.');
+            $('#jokeDelivery').text('');
+          } else {
+            $('#jokeSetup').text(data.setup);
+            $('#jokeDelivery').text(data.delivery);
+          }
+        })
+        .fail(function () {
+          $('#jokeSetup').text('An error occurred while fetching the joke.');
+          $('#jokeDelivery').text('');
+          console.error('Error fetching joke.');
+        });
+    } else {
+      $('#jokeSetup').text('Please select at least one category.');
+      $('#jokeDelivery').text('');
+    }
+  });
 });
